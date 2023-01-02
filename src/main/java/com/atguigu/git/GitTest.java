6252cf852bf5784301de077dabdd77a62b1d11a6
codkiller <95393244+codkiller@users.noreply.github.com>
2023-01-02 13:24:52 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,8 @@ public class GitTest {
         System.out.println("master test2!");
         System.out.println("hot-fix test2!");
         System.out.println("push test!");
+        System.out.println("pull test!");
+       
 
     }
 }
